697923a5d53727326c44674043372d4ee4e44714
Lev Zlobin <tss.edits@gmail.com>
2023-05-08 14:24:50 +0300

    added: Profile.username

--- api/models.py
+++ api/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=60)
     name = models.CharField(max_length=20, blank=True, null=True)
     surname = models.CharField(max_length=20, blank=True, null=True)
     description = models.CharField(max_length=200, blank=True, null=True)
