e8b4e6ecb30755b222dbf3dc0da970e90051fcda
Lev Zlobin <tss.edits@gmail.com>
2023-05-08 14:27:38 +0300

    added: TokenUser, ProfileData views; update: migrations ignored by git

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from api import views
 urlpatterns = [
     path('register/', views.UserRegister, name='UserRegister'),
     path('login/', views.UserLogin, name='UserLogin'),
-    path('tokendata/', views.TokenFromCookieData, name='TokenFromCookieVerification'),
+    path('token/', views.TokenUser, name='TokenUser'),
+    path('<str:username>/profile/', views.ProfileData, name='ProfileData'),
+    # path('<str:username>/tracks/', views.TracksData, name='TracksData'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,11 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from api import tokendata
-from server.settings import SECRET_KEY as secret
+from api.models import Profile, TrackToUser
 from rest_framework_jwt.utils import jwt_payload_handler
 import json
-import jwt
 
-from requests import Response
+
 
 # Create your views here.
 def UserRegister(request):
@@ -17,8 +16,18 @@ def UserRegister(request):
 
         if User.objects.filter(email=data['email']):
             return JsonResponse({'result':'failed', 'error':'EmailUsed'}, safe=False)
+             
+        user = User()
+        user.username = data['username']
+        user.email = data['email']
+        user.set_password(data['password'])
+        user.save()
+
+        profile = Profile()
+        profile.user = user
+        profile.username = data['username']
+        profile.save()
         
-        User.objects.create_user(username=data['username'], email=data['email'], password=data['password'])
         return JsonResponse({'result':'success'}, safe=False)
 
     return JsonResponse({'result':'failed', 'error':'WrongMethod'}, safe=False)
@@ -41,8 +50,7 @@ def UserLogin(request):
         
         print(f'YOUR USER IS: {user}')
 
-        payload = jwt_payload_handler(user)
-        token = jwt.encode(payload, secret, algorithm='HS256').decode('utf-8')
+        token = tokendata.token_generate(user)
         print(f'YOUR TOKEN IS: {token}')
         response = JsonResponse({'result':'success'}, safe=False)
         response.set_cookie(key='JWT', value=token, max_age=3600, httponly=True)
@@ -51,14 +59,45 @@ def UserLogin(request):
     return JsonResponse({'result':'failed', 'error':'WrongMethod'}, safe=False)
 
 
-def TokenFromCookieData(request):
+
+def TokenUser(request):
     if request.method == 'GET':    
-        payload = tokendata.from_cookie_data(request, secret)
+        payload = tokendata.from_cookie_token_data(request)
         if payload is None:
             return JsonResponse({'result':'failed','error':'TokenVerificationFailed'}, safe=False)
-        return JsonResponse(payload, safe=False)
+        return JsonResponse({'result':'success', 'user':payload}, safe=False)
 
     return JsonResponse({'result':'failed', 'error':'WrongMethod'}, safe=False)
+
+
+
+def ProfileData(request, username):
+    if request.method == 'GET':
+        payload = tokendata.from_cookie_token_data(request)
+        if payload is None:
+            return JsonResponse({'result':'failed','error':'TokenVerificationFailed'}, safe=False)
+
+        try:
+            profile = Profile.objects.get(username=username)
+        except:
+            return JsonResponse({'result':'failed', 'error':'ProfileDoesNotExist', 'user':payload}, safe=False)
+
+        data = {
+            'user':profile.username,
+            'name':profile.name,
+            'surname':profile.surname,
+            'description':profile.description
+        }
+
+        return JsonResponse({'result':'success', 'user':payload, 'data':data}, safe=False)
+
+
+# def TracksData(request, username):
+#     if request.method == 'GET':
+#         user = User.objects.get(username=username)
+#         print(user)
+#         print(user.tracks.values())
+#         return HttpResponse(200)
 
 
 
